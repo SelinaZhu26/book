45ba1def31826f25fedbaebe1f062d175b9da5d5
jermaine <opsbyjemz@gmail.com>
2026-03-09 17:31:16 -0400

    Initial Commit

---
+++ book/bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookApp.swift
+//  book
+//
+//  Created by BMCC Lab Guest on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
